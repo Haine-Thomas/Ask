f0a816ec24b1534e8c16e6c027f6911d41b7cfaf
Mushi2216 <thomahaine@gmail.com>
2020-03-17 09:24:35 +0100

    correction and additional datas in table

--- data/import_data.sql
+++ data/import_data.sql
@@ -4,11 +4,20 @@
 
 BEGIN;
 
-INSERT INTO "user" ("id", "name", "email", "password", "created_at", "avatar") VALUES
-(1, "Mushi", "thomahaine@gmail.com", "ojajayapasmoyenjaja", '2020-03-13 12:15:33', "http://icons.iconarchive.com/icons/diversity-avatars/avatars/96/batman-icon.png"),
+INSERT INTO "user" ("user_id", "name", "email", "password","admin" "created_at") VALUES
+(1, "Mushi", "thomahaine@gmail.com", "ojajayapasmoyenjaja", "true", '2020-03-13 12:15:33');
 
-INSERT INTO "question"("id", "subject", "created_at", "user_account_id") VALUES
-(1, "Est-ce que les chats peuvent nous tuer?", '2020-03-13 15:15:33', 1)
+INSERT INTO "question"("question_id", "content", "created_at", "user_id", 'upvote', 'downvote') VALUES
+(1, "Est-ce que les chats peuvent nous tuer?", '2020-03-13 15:15:33', 1, 0, 0);
 
-INSERT INTO "posts"("id","content", "created_at", "question_id","user_account_id") VALUES
-(1, "Pour sûr il peuvent t'arracher les yeux", '2020-03-13 15:30:00', 1, 1)
+INSERT INTO "answer"("answer_id","content", "created_at", "question_id","user_id", 'upvote', 'downvote') VALUES
+(1, "Pour sûr il peuvent t'arracher les yeux", '2020-03-13 15:30:00', 1, 1, 0, 0);
+
+INSERT INTO "tag"('tag_id', 'name', 'status') VALUES
+(1, "DRÔLE", "true"),
+(2,"CREEPY", "true"),
+(3, "SERIEUX", "true"),
+(4, "+18", "false");
+
+INSERT INTO 'question_has_tag' ('tag_id', 'question_id') VALUES
+(3,1);
--- data/import_tables.sql
+++ data/import_tables.sql
@@ -8,14 +8,14 @@
 DROP TABLE IF EXISTS "user" ;
 
 CREATE TABLE IF NOT EXISTS "user" (
-  "id" SERIAL NOT NULL,
+  "user_id" SERIAL NOT NULL,
   "name" VARCHAR(50) NOT NULL,
   "email" VARCHAR(255) NOT NULL,
   "password" VARCHAR(50) NOT NULL,
   "admin" BOOLEAN DEFAULT 0 ,
   "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "avatar" URL NOT NULL,
   PRIMARY KEY ("id"));
+
 
 
 -- -----------------------------------------------------
@@ -24,21 +24,52 @@ CREATE TABLE IF NOT EXISTS "user" (
 DROP TABLE IF EXISTS "question" ;
 
 CREATE TABLE IF NOT EXISTS "question" (
-  "id" SERIAL NOT NULL,
-  "subject" VARCHAR(100) NOT NULL,
+  "question_id" SERIAL NOT NULL,
+  "content" VARCHAR(100) NOT NULL,
   "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "user_account_id" INT NOT NULL,
+  "user_id" INT NOT NULL,
+  "upvote" INT NOT NULL,
+  "downvote" INT NOT NULL,
   PRIMARY KEY ("id"));
 
--- -----------------------------------------------------
--- Table "posts"
--- -----------------------------------------------------
-DROP TABLE IF EXISTS "posts" ;
 
-CREATE TABLE IF NOT EXISTS "posts" (
-  "id" SERIAL NOT NULL,
+
+-- -----------------------------------------------------
+-- Table "answer"
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS "answer" ;
+
+CREATE TABLE IF NOT EXISTS "answer" (
+  "answer_id" SERIAL NOT NULL,
   "content" VARCHAR(1000) NOT NULL,
   "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "question_id" INT NOT NULL,
-  "user_account_id" INT NOT NULL,
+  "user_id" INT NOT NULL,
+  "upvote" INT NOT NULL,
+  "downvote" INT NOT NULL,
   PRIMARY KEY ("id"));
+
+
+-- -----------------------------------------------------
+-- Table "tag"
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS "tag" ;
+
+CREATE TABLE IF NOT EXISTS "tag" (
+  "tag_id" SERIAL NOT NULL,
+  "name" VARCHAR(60) NOT NULL,
+  "status" BOOLEAN NOT NULL,
+  PRIMARY KEY ("id"));
+
+
+
+-- -----------------------------------------------------
+-- Table "question_has_tag"
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS "question_has_tag" ;
+
+CREATE TABLE IF NOT EXISTS "question_has_tag" (
+  "question_id" SERIAL NOT NULL,
+  "tag_id" SERIAL NOT NULL,
+  PRIMARY KEY ("question_id", "tag_id"));
